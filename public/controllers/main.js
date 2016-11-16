5cbac15a44e6468462408f9b7dcaba04de03b2c9
Sayali Patkar <sayali.patkar@student.fh-kiel.de>
2016-11-16 22:21:54 +0100

    Now we can run on Server!!

---
+++ public/controllers/main.js
@@ -0,0 +1,43 @@
+/**
+ * Created by SAYALI on 16-11-2016.
+ */
+angular.module('MyApp')
+    .controller('MainCtrl', function() {
+
+
+        /*function findUser()
+        {
+            var i=0;
+            // if user is login i=1
+            if(i==0)
+            {
+                //arrow
+                var img1 = document.getElementById('img1');
+                img1.style.display = "none";
+                var img2 = document.getElementById('img2');
+                img2.style.display = "block";
+                document.getElementById('btnsignout').style.display="none";
+                document.getElementById('txtLogin').style.display="block";
+                document.getElementById('btnChannels').style.pointerEvents="none";
+            }
+            if(i!=0)
+            {
+                //login
+                var img1 = document.getElementById('img1');
+                img1.style.display = "block";
+                var img2 = document.getElementById('img2');
+                img2.style.display = "none";
+
+                document.getElementById('btnsignout').style.display="block";
+                document.getElementById('txtLogin').style.display="none";
+                document.getElementById('btnChannels').style.pointerEvents="all";
+            }
+        }
+        function clickArrow() {
+
+            window.alert("sometext");
+            //      window.scrollTo(2000, 0);
+        }*/
+        window.alert("In home!!")
+
+    });
