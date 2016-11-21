5a5001195822091b30fb61757305d0992c7873c2
Sayali Patkar <sayali.patkar@student.fh-kiel.de>
2016-11-21 22:26:13 +0100

    MongoDb database schemas added

--- server.js
+++ server.js
@@ -16,3 +16,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+var showSchema = new mongoose.Schema({
+  _id: {type: String, unique: true},
+  name: String,
+  airsDaysOfWeek: [String],
+  airsTime: String,
+  firstAired: Date,
+  genre: [String],
+  network: String,
+  overview: String,
+  image: String
+});
+
+var userSchema = new mongoose.Schema({
+  _id: {type: String, unique: true},
+  subscriptions : [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'Show'
+  }]
+});
+
+var User = mongoose.model('User', userSchema);
+var Show = mongoose.model('Show', showSchema);
+
+mongoose.connect('localhost');
