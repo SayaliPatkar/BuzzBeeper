8f26455d8b0cbd738b4dd064dac10c3d22cfd5c3
Tanya Gupta <tanya.gupta@student.fh-kiel.de>
2016-11-22 23:38:37 +0100

    update in user page for json

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -5,11 +5,12 @@ angular.module('MyApp')
     .controller('UserCtrl', function() {
 
         document.getElementById('myNavbar').innerHTML = '<ul class="nav navbar-nav navbar-right"> <li data-match-route="/userpage"><a href="/userpage">Home</a></li> <li data-match-route="/channels"><a href="/channels">Channels</a></li> <li data-match-route="/contactus"><a href="/contactus">Contact Us</a></li><ul class="nav navbar-nav pull-right" id="logoutPane"> <li><a onclick="logout()" href="javascript:void(0);"">Logout</a></li> </ul> </ul>';
- var data ='[{ "id" : "1","image":"../images/Demo2.jpg", "name" : "Game of thrones","airsDaysOfWeek" : ["Monday","Wednesday"], "airsTime": "11:00 am",	"firstAired":"2013-05-05 11:00",	"genre":"action",	"network":"abc","overview":"description description description description description description description description description"},{"id" : "2","image":"../images/Demo1.jpg","name" : "Friends","airsDaysOfWeek" : ["Monday","Wednesday","Saturday"],"airsTime": "1:00 am","firstAired":"2013-05-05 1:00","genre":"action","network":"abc","overview":"description description description description description description description description description"},{ "id" : "3","image":"../images/Demo3.jpg", "name" : "XYZ","airsDaysOfWeek" : ["Monday","Wednesday"], "airsTime": "11:00 am",	"firstAired":"2013-05-05 11:00",	"genre":"action",	"network":"abc","overview":"description description description description description description description description description"},{"id" : "4","image":"../images/demo4.jpg","name" : "Friends","airsDaysOfWeek" : ["Monday","Wednesday","Saturday"],"airsTime": "1:00 am","firstAired":"2013-05-05 1:00","genre":"action","network":"abc","overview":"description description description description description description description description description"}]';
-       var json = JSON.parse(data);
+        var data ='[{ "id" : "1","image":"../images/Demo2.jpg", "name" : "Game of thrones","airsDaysOfWeek" : ["Monday","Wednesday"], "airsTime": "11:00 am",	"firstAired":"2013-05-05 11:00",	"genre":"action",	"network":"abc","overview":"description description description description description description description description description"},{"id" : "2","image":"../images/Demo1.jpg","name" : "Friends","airsDaysOfWeek" : ["Monday","Wednesday","Saturday"],"airsTime": "1:00 am","firstAired":"2013-05-05 1:00","genre":"action","network":"abc","overview":"description description description description description description description description description"},{ "id" : "3","image":"../images/Demo3.jpg", "name" : "XYZ","airsDaysOfWeek" : ["Monday","Wednesday"], "airsTime": "11:00 am",	"firstAired":"2013-05-05 11:00",	"genre":"action",	"network":"abc","overview":"description description description description description description description description description"},{"id" : "4","image":"../images/demo4.jpg","name" : "Friends","airsDaysOfWeek" : ["Monday","Wednesday","Saturday"],"airsTime": "1:00 am","firstAired":"2013-05-05 1:00","genre":"action","network":"abc","overview":"description description description description description description description description description"}]';
+        var json = JSON.parse(data);
 
 
         for(var i=0;i<json.length;i++) {
+
             var sub=json[i].overview.substring(0,60);
             document.getElementById('createList').innerHTML += '<tr><td style="padding: 4px !important;"><img src="'+json[i].image+'" height="500" width="393" style="height: 250px;width: 240px;"/></td><td>'+
                 '<table><tr><td ><p class="caroselP">'+json[i].name+'</p></td></tr>'+'' +
@@ -17,6 +18,26 @@ angular.module('MyApp')
                 '<p class="caroselPoverview" maxlength="1">'+sub+'</p></td></tr><tr><td><button class="readMoreBtn">Read More</button></td></tr></table>'+
                 '</td></tr>';
 
+        }
+       // alert(json.length);
+        if(json.length==5)
+        {
+            $("#createList").css("margin-top", "800px");
+
+        }
+        else
+        if(json.length==4)
+        {
+            $("#createList").css("margin-top", "600px");
+        }
+        else
+        if(json.length==3)
+        {
+            $("#createList").css("margin-top", "370px");
+        }
+        if(json.length==2)
+        {
+            $("#createList").css("margin-top", "150px");
         }
 
     });
