bd49b38755be7dbabf6073bd08b9c8d7bd5ac7a4
Sayali Patkar <sayali.patkar@student.fh-kiel.de>
2016-11-22 01:06:04 +0100

    CSS added

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -4,6 +4,6 @@
 angular.module('MyApp')
     .controller('UserCtrl', function() {
 
-        document.getElementById('myNavbar').innerHTML = '<ul class="nav navbar-nav navbar-right"> <li data-match-route="/$"><a href="/">Home</a></li> <li data-match-route="/channels"><a href="/channels">Channels</a></li> <li data-match-route="/contactUs"><a href="/contactUs">Contact Us</a></li><ul class="nav navbar-nav pull-right" id="logoutPane"> <li><a onclick="logout()" href="javascript:void(0);"">Logout</a></li> </ul> </ul>';
+        document.getElementById('myNavbar').innerHTML = '<ul class="nav navbar-nav navbar-right"> <li data-match-route="/userpage"><a href="/userpage">Home</a></li> <li data-match-route="/channels"><a href="/channels">Channels</a></li> <li data-match-route="/contactus"><a href="/contactus">Contact Us</a></li><ul class="nav navbar-nav pull-right" id="logoutPane"> <li><a onclick="logout()" href="javascript:void(0);"">Logout</a></li> </ul> </ul>';
 
     });
