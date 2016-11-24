2a2a07307f3734d089c58c0a801e63d1f0a64ff4
anushavenkapally <anu.venkapally@gmail.com>
2016-11-24 19:19:42 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var async = require('async');
+var request = require('request');
+var xml2js = require('xml2js');
+var _ = require('lodash');
 
 var app = express();
 
@@ -27,9 +31,94 @@ app.get('/api/shows', function(req, res, next) {
 });
 
 app.get('/api/shows/:id', function(req, res, next) {
-  Show.findById(req.params.id, function(err, show) {
+    Show.findById(req.params.id, function(err, show) {
     if (err) return next(err);
     res.send(show);
+  });
+});
+
+app.post('/api/shows', function(req, res, next) {
+  var apiKey = '9EF1D1E7D28FDA0B';
+  console.log("inside post");
+  var parser = xml2js.Parser({
+    explicitArray: false,
+    normalizeTags: true
+  });
+  console.log("inside post parser conigured");
+  var seriesName = req.body.showName
+      .toLowerCase()
+      .replace(/ /g, '_')
+      .replace(/[^\w-]+/g, '');
+  console.log(seriesName);
+
+
+  async.waterfall([
+    function(callback) {
+      request.get('http://thetvdb.com/api/GetSeries.php?seriesname=' + seriesName, function(error, response, body) {
+        if (error) return next(error);
+        parser.parseString(body, function(err, result) {
+          if (!result.data.series) {
+            return res.send(404, { message: req.body.showName + ' was not found.' });
+          }
+          var seriesId = result.data.series.seriesid || result.data.series[0].seriesid;
+          callback(err, seriesId);
+        });
+      });
+    },
+    function(seriesId, callback) {
+      request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesId + '/all/en.xml', function(error, response, body) {
+        if (error) return next(error);
+        parser.parseString(body, function(err, result) {
+          var series = result.data.series;
+          var episodes = result.data.episode;
+          console.log(series.seriesname);
+          var show = new Show({
+            _id: series.id,
+            name: series.seriesname,
+            airsDayOfWeek: series.airs_dayofweek,
+            airsTime: series.airs_time,
+            firstAired: series.firstaired,
+            genre: series.genre.split('|').filter(Boolean),
+            network: series.network,
+            overview: series.overview,
+            rating: series.rating,
+            ratingCount: series.ratingcount,
+            runtime: series.runtime,
+            status: series.status,
+            poster: series.poster,
+            episodes: []
+          });
+          _.each(episodes, function(episode) {
+            show.episodes.push({
+              season: episode.seasonnumber,
+              episodeNumber: episode.episodenumber,
+              episodeName: episode.episodename,
+              firstAired: episode.firstaired,
+              overview: episode.overview
+            });
+          });
+          callback(err, show);
+        });
+      });
+    },
+    function(show, callback) {
+      var url = 'http://thetvdb.com/banners/' + show.poster;
+      request({ url: url, encoding: null }, function(error, response, body) {
+        show.poster = 'data:' + response.headers['content-type'] + ';base64,' + body.toString('base64');
+        callback(error, show);
+      });
+    }
+  ], function(err, show) {
+    if (err) return next(err);
+    show.save(function(err) {
+      if (err) {
+        if (err.code == 11000) {
+          return res.send(409, { message: show.name + ' already exists.' });
+        }
+        return next(err);
+      }
+      res.send(200);
+    });
   });
 });
 
@@ -39,7 +128,7 @@ app.use(function(err, req, res, next) {
 });
 
 app.get('*', function(req, res) {
-    res.redirect('/#' + req.originalUrl);
+  res.redirect('/#' + req.originalUrl);
 });
 
 var mongoose = require('mongoose');
@@ -54,7 +143,7 @@ var showSchema = new mongoose.Schema({
   genre: [String],
   network: String,
   overview: String,
-  image: String
+  poster: String
 });
 
 var userSchema = new mongoose.Schema({
