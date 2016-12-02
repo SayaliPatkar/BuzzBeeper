302a1dde9c1f13da0f2ded3d6295dd29bc0e31be
Tanya Gupta <tanya.gupta@student.fh-kiel.de>
2016-12-02 18:26:51 +0100

    Merge branch 'master' of C:\Users\Administrator\Documents\BB3011\BuzzBeeper with conflicts.

--- public/controllers/showcontroller.js
+++ public/controllers/showcontroller.js
@@ -2,8 +2,10 @@
  * Created by SAYALI on 16-11-2016.
  */
 angular.module('MyApp')
-    .controller('ShowCtrl', function() {
+    .controller('ShowCtrl', ['$scope', 'Show', function($scope, Show) {
 
+         $scope.headingTitle = 'All Shows so far in Database';
 
+        $scope.shows = Show.query();
 
-    });
+    }]);
