514585c8d7491492ecc7372b65e6e8c41e7e9d76
Tanya Gupta <tanya.gupta@student.fh-kiel.de>
2016-12-08 19:02:42 +0100

    commented code with information about angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,11 @@
-/**
- * Created by SAYALI on 16-11-2016.
- */
+
+
+//ngCookies-  for reading and writing browser cookies.
+//ngResource- provides interaction support with RESTful services via the $resource service
+//ngMessages- is a directive that is designed to show and hide messages based on the state of a key/value object that it listens on
+//ngRoute- module provides routing and deeplinking services
+//mgcrea.ngStrap- AngularStrap is a set of native directives that enables seamless integration of Bootstrap#^3.0 into your AngularJS#^1.2 application
+
 angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
     .config(['$locationProvider','$routeProvider',function($locationProvider, $routeProvider) {
         $locationProvider.html5Mode(true);
